d81b603ffd8f1df65506d5c1f82b7a4465adc15f
mitkokazakov <DimitarKazakov@students.softuni.bg>
2023-08-29 19:17:17 +0300

    Add DTO needed for submitting new recipe

---
+++ RecipeWebApp/RecipeWebApp/DTO/AddRecipeFormSubmit.cs
@@ -0,0 +1,19 @@
+﻿namespace RecipeWebApp.DTO
+{
+    public class AddRecipeFormSubmit
+    {
+        public string RecipeName { get; set; }
+
+        public int CookingTime { get; set; }
+
+        public int Servings { get; set; }
+
+        public string Category { get; set; }
+
+        public string AllIngredients { get; set; }
+
+        public string Instructions { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+}
