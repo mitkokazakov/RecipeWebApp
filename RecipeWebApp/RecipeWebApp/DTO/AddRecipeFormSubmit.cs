147c23a9f30e54c74b31037c39ea97b17d704c3d
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-09-05 08:51:57 +0300

    Make separate static class for constants in AddRecipeFormModel

--- RecipeWebApp/RecipeWebApp/DTO/AddRecipeFormSubmit.cs
+++ RecipeWebApp/RecipeWebApp/DTO/AddRecipeFormSubmit.cs
@@ -1,30 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static RecipeWebApp.DTO.FormModelsConstants;
 
 namespace RecipeWebApp.DTO
 {
     public class AddRecipeFormSubmit
     {
         [Required]
-        [MinLength(2,ErrorMessage ="Recipe Name should be at least 2 characters long!")]
+        [MinLength(RecipeNameMinLen,ErrorMessage ="{0} should be at least {1} characters long!")]
         public string RecipeName { get; set; } = null!;
 
         [Required]
-        [MinLength(5, ErrorMessage = "Cooking Time should last at least 5 minutes!")]
+        [MinLength(CookingMinTime, ErrorMessage = "{0} should last at least {1} minutes!")]
         public int CookingTime { get; set; }
 
         [Required]
-        [MinLength(1, ErrorMessage = "Servings should last at least 1!")]
+        [MinLength(ServingsMinCount, ErrorMessage = "{0} should be at least {1}!")]
         public int Servings { get; set; }
 
         [Required]
         public string Category { get; set; } = null!;
 
         [Required]
-        [MinLength(10, ErrorMessage = "Ingredients should be at least 10 characters long!")]
+        [MinLength(AllIngredinetsMinLen, ErrorMessage = "{0} should be at least {1} characters long!")]
         public string AllIngredients { get; set; } = null!;
 
         [Required]
-        [MinLength(10,ErrorMessage ="Instructions should be at least 10 characters long!")]
+        [MinLength(InstructionsMinLen,ErrorMessage ="{0} should be at least {1} characters long!")]
         public string Instructions { get; set; } = null!;
 
         [Required]
---
+++ RecipeWebApp/RecipeWebApp/DTO/FormModelsConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipeWebApp.DTO
+{
+    public static class FormModelsConstants
+    {
+        public const int RecipeNameMinLen = 2;
+        public const int CookingMinTime = 5;
+        public const int ServingsMinCount = 1;
+        public const int AllIngredinetsMinLen = 10;
+        public const int InstructionsMinLen = 10;
+    }
+}
