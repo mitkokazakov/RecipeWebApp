cf5385bb816f20af5238fe41142c98594bc70684
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-09-05 11:06:48 +0300

    Add logic for adding recipe

--- RecipeWebApp/RecipeWebApp/Services/RecipeService.cs
+++ RecipeWebApp/RecipeWebApp/Services/RecipeService.cs
@@ -1,4 +1,5 @@
 ﻿using RecipeWebApp.DTO;
+using RecipeWebApp.Data.Models;
 
 namespace RecipeWebApp.Services
 {
@@ -6,7 +7,46 @@ namespace RecipeWebApp.Services
     {
         public Task AddRecipe(AddRecipeFormSubmit model)
         {
-            throw new ArgumentException();
+            string extension = Path.GetExtension(model.Image.FileName);
+
+            Image image = new Image()
+            {
+                Name = Guid.NewGuid().ToString(),
+                Extension = extension
+            };
+
+            var ingredientsArray = SplitIngredients(model.AllIngredients);
+
+            Recipe recipe = new Recipe()
+            {
+                Name = model.RecipeName,
+                Instructions = model.Instructions,
+                Image = image,
+                Category = model.Category,
+                CookingTime = model.CookingTime,
+                Servings = model.Servings,
+                CreatedOn = DateTime.UtcNow,
+                Ingridients = ingredientsArray[0],
+            };
+        }
+
+        private string[] SplitIngredients(string allIngredients) 
+        {
+            var arr = allIngredients.Split("\r\n\r\n");
+
+            return arr;
+        }
+
+        private List<SubIngredient> SubIngredientsSplit(string[] allIngredients) 
+        {
+            List<SubIngredient> subIngredients = new List<SubIngredient>();
+
+            foreach (var subIngr in allIngredients.Skip(1))
+            {
+
+            }
+
+            return subIngredients;
         }
     }
 }
