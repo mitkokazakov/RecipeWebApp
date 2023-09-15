886ede622923eb84b6bfeb2eb9d02c949d67066b
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2023-09-15 12:05:30 +0300

    Add confirmation modal for deleting a recipe

--- RecipeWebApp/RecipeWebApp/wwwroot/js/site.js
+++ RecipeWebApp/RecipeWebApp/wwwroot/js/site.js
@@ -14,3 +14,28 @@ function onClickBurgerMenu() {
 
 burgerMenuRef.addEventListener('click', onClickBurgerMenu);
 
+
+
+let deleteRecipeBtn = document.querySelector('.delete-recipe-btn');
+let confirmDeleteBtn = document.querySelector('.confirm-delete');
+let closeDeletePopupBtn = document.querySelector('.close-delete-popup');
+let popupDeleteModal = document.querySelector('.popup-delete');
+
+function onClickDeleteRecipeBtn()
+{
+    console.log('ok');
+    if (!popupDeleteModal.classList.contains('open')) {
+        popupDeleteModal.classList.add('open');
+    }
+}
+
+function onClickCloseModalBtn() {
+    if (popupDeleteModal.classList.contains('open')) {
+        popupDeleteModal.classList.remove('open');
+    }
+}
+
+deleteRecipeBtn.addEventListener('click', onClickDeleteRecipeBtn);
+closeDeletePopupBtn.addEventListener('click', onClickCloseModalBtn);
+
+
